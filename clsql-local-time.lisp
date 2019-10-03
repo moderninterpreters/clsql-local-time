9fad28a696a9a239243b4f186c31aec7dc55fd28
Arnold Noronha <arnold@tdrhq.com>
2019-10-03 15:38:10 -0700

    add the code

--- clsql-local-time.lisp
+++ clsql-local-time.lisp
@@ -1,3 +1,13 @@
 ;;;; clsql-local-time.lisp
 
 (in-package #:clsql-local-time)
+
+(defmethod clsql-sys:read-sql-value (val (type (eql 'local-time:timestamp))  database db-type)
+  (let ((wall-time (clsql-sys:parse-timestring val)))
+    (multiple-value-bind (usec sec min hour day month year dow)
+        (clsql-sys:decode-time wall-time)
+      (let ((ret (local-time:encode-timestamp (* usec 1000) sec min hour day month year :timezone local-time:+utc-zone+)))
+        ret))))
+
+(defmethod clsql-sys::database-output-sql ((time local-time:timestamp) database)
+  (clsql-sys::database-output-sql (local-time:format-timestring nil time :format '(:year "-" :month "-" :day " " :hour ":" :min ":" :sec " " :timezone ) :timezone local-time:+utc-zone+) database))
